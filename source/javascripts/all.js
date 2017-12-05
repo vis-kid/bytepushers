3ee58116062475f3cfdaf76cfe164cfd543e2b55
ed wassermann <vis0vis@gmail.com>
2017-12-05 01:12:44 +0100

    Implement hamburger menu
    WIP
    Hamburger only (for now)
    Add styles
    Add jQuery function
    Move jQuery links into header

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,15 +1,8 @@
-var $ = require('jQuery');
-
-$(document).ready(function() {
-  var menuToggle;
-  menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
-  $('#js-centered-navigation-menu').removeClass('show');
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-centered-navigation-menu').slideToggle(function() {
-      if ($('#js-centered-navigation-menu').is(':hidden')) {
-        $('#js-centered-navigation-menu').removeAttr('style');
-      }
-    });
+$( document ).ready(function() {
+  $(".menu-toggle").on('click', function() {
+    $(this).toggleClass("on");
+      $('.header-border').toggleClass("menu-spacer");
+      $('.menu-section').toggleClass("on");
+      $("nav ul").toggleClass('hidden');
   });
 });
