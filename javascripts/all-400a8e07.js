0cba752f7173f49e4ac88756f3af45cc79a763d3
ed wassermann <vis0vis@gmail.com>
2017-12-05 02:56:48 +0100

    Automated commit at 2017-12-05 01:56:48 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all-400a8e07.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".menu-toggle").on("click",function(){$(this).toggleClass("on"),$(".header-border").toggleClass("menu-spacer"),$(".menu-section").toggleClass("on"),$("nav ul").toggleClass("hidden")})}),$(document).ready(function(){alert("document loaded")});
---
+++ javascripts/all-79122d89.js
@@ -0,0 +1,1 @@
+$(function(){$(".menu-toggle").on("click",function(){$(this).toggleClass("on"),$(".header-border").toggleClass("menu-spacer"),$(".menu-section").toggleClass("on"),$("nav ul").toggleClass("hidden")})});
